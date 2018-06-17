30ebb064abfbdd13e08127933e0a4b667530de1f
Papa Mbengue <papa-takhy.mbengue@epita.fr>
2018-06-17 20:25:26 +0200

    test up

--- test.py
+++ test.py
@@ -35,7 +35,7 @@ class Test(unittest.TestCase):
     self.assertEqual(res, 4)
   def test_div(self) :
     res = divide(4,2)
-    self.assertEqual(res, 2)
+    self.assertEqual(res, 1)
 
 unittest.main()
 #calc = Calculator()
