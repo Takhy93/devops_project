200866c3aaf5ce28d59cc208f163260615a075f2
Papa Mbengue <papa-takhy.mbengue@epita.fr>
2018-06-17 17:56:20 +0200

    test file

---
+++ test.py
@@ -0,0 +1,42 @@
+import random
+import unittest
+
+class RandomTest(unittest.TestCase):
+  def test_choice(self):
+    liste = list(range(10))
+    elt = random.choice(liste)
+    self.assertIn(elt, liste)
+
+def add(x, y):
+  return x + y
+  
+       # This function subtracts two numbers
+def subtract(x, y):
+  return x - y
+
+  # This function multiplies two numbers
+
+def multiply(x, y):
+  return x * y
+
+   # This function divides two numbers
+def divide(x, y):
+  return x / y
+
+class Test(unittest.TestCase):
+  def test_add(self):
+    res = add(2,3)
+    self.assertEqual(res, 5)
+  def test_sub(self) :
+    res = subtract(1,1)
+    self.assertEqual(res, 0)
+  def test_mult(self):
+    res = multiply(2,2)
+    self.assertEqual(res, 4)
+  def test_div(self) :
+    res = divide(4,2)
+    self.assertEqual(res, 2)
+
+unittest.main()
+#calc = Calculator()
+#self.assertEqual(5,5)
