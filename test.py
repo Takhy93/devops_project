93cf3ce0c7b9b93825cd2b7e79edc3b2b0cacd8d
Papa Mbengue <papa-takhy.mbengue@epita.fr>
2018-06-18 17:46:20 +0200

    test

--- test.py
+++ test.py
@@ -1,36 +1,18 @@
 import random
 import unittest
-
-def add(x, y):
-  return x + y
-  
-       # This function subtracts two numbers
-def subtract(x, y):
-  return x - y
-
-  # This function multiplies two numbers
-
-def multiply(x, y):
-  return x * y
-
-   # This function divides two numbers
-def divide(x, y):
-  return x / y
+import urllib
 
 class Test(unittest.TestCase):
-  def test_add(self):
-    res = add(2,3)
-    self.assertEqual(res, 5)
-  def test_sub(self) :
-    res = subtract(1,1)
-    self.assertEqual(res, 0)
-  def test_mult(self):
-    res = multiply(2,2)
-    self.assertEqual(res, 4)
-  def test_div(self) :
-    res = divide(4,2)
-    self.assertEqual(res-1, 2)
-
+  def test_adduser(self):
+    res = urllib.urlopen("http://127.0.0.1:5000/add_user").getcode()
+    self.assertEqual(res, 200)
+  def test_show_user(self) :
+    res = urllib.urlopen("http://127.0.0.1:5000/show_user/1").getcode()
+    self.assertEqual(res, 200)
+  def test_list_user(self):
+    res = urllib.urlopen("http://127.0.0.1:5000/list_user").getcode()
+    self.assertEqual(res, 200)
+  
 
 if __name__ == '__main__':
   unittest.main()
